4ee41eda3cc4eb6a03efa3b3c965453548f44b2a
student1 <dima.romakhov@gmail.com>
2022-10-07 14:30:40 +0300

    intermediate fix

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const inputTask = document.querySelector(".todo__enter");
+// inputTask.addEventListener("change", inputHandler);
+
+const addTask = document.querySelector(".todo__add");
+addTask.addEventListener("click", inputHandler);
+
+const taskPlace = document.querySelector(".todo__main");
+
+function inputHandler() {
+  let taskText = inputTask.value;
+  console.log(taskText);
+  createTask();
+}
+
+function createTask() {
+    
+}
